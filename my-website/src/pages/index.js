ccfb46f85ff16e0f83cbe62557ed876b6cddfd1a
Yoofi-A-Appiah <appiahyoofi@gmail.com>
2022-07-26 21:17:56 +0000

    Propeller Ads

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -7,9 +7,22 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import  { Redirect } from 'react-router-dom';
 
 import styles from './index.module.css';
+import Head from '@docusaurus/Head';
 
 
 export default function Home() {
-  return <Redirect to='/blog' />;
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout>
+      <Head>
+      <meta name="propeller" content="ad5b38b95f726654cab26a16d857bdf9" />
+              </Head>
+              <Redirect to='/blog' />;
+
+    </Layout>
+    
+  );
+  return 
 
 }
